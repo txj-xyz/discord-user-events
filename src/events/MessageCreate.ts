d609e8ae9a0496fbdadf541d0cbab4df936e3788
txj-xyz <jfinelli3@gmail.com>
2022-08-10 10:48:02 -0600

    new client logger handler

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -1,8 +1,10 @@
-import { Client, Message } from 'discord.js-selfbot-v13';
-
+import { Client, Message } from "discord.js-selfbot-v13";
+import { logger } from "../modules/Logger";
 
 export default function message(client: Client) {
-    client.on("messageCreate", function (message: Message) {
-        console.log(message)
-    })
+  client.on("messageCreate", function (message: Message) {
+    logger.log({
+      message: message.content,
+    });
+  });
 }
