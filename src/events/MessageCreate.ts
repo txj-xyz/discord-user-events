e32f5144466604845078b7112544a830fc7655fb
txj-xyz <jfinelli3@gmail.com>
2022-08-10 11:56:11 -0600

    fetch users etc

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -1,10 +1,11 @@
-import { Client, Message } from "discord.js-selfbot-v13";
+import { Client, ClientUser, Guild, GuildMember, Message, User } from "discord.js-selfbot-v13";
 import { logger } from "../modules/Logger";
 
 export default function message(client: Client) {
-  client.on("messageCreate", function (message: Message) {
-    logger.log({
-      message: message.content,
-    });
+    client.on("messageCreate", async function (message: Message) {
+        const guild: Guild | undefined = client.guilds.cache.find(e => e.id === message.guildId);
+        // const user: User | undefined = client?.users.cache.find(e => e.id === message.author.id);
+        const guilduser: GuildMember | undefined = guild?.members.cache.find(e => e.id === message.author.id);
+        logger.log(`user is: ${guilduser?.user.username}`);
   });
 }
