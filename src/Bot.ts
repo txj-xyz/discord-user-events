b46c21f5e9f5de53af397f5fc4e7b2e8bd5e1926
txj-xyz <jfinelli3@gmail.com>
2022-08-10 10:47:46 -0600

    more imports better logging

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,23 +1,24 @@
 // Setup .env file here
-require('dotenv').config();
+require("dotenv").config();
 
 // Check token before anything else
-if(!process.env.TOKEN) throw new Error('TOKEN missing, please add a user token.')
+if (!process.env.TOKEN)
+  throw new Error("TOKEN missing, please add a user token.");
 
 // Import Discord Selfbot Lib
-import { Client, Intents } from 'discord.js-selfbot-v13';
-import message from './events/MessageCreate';
-import ready from './events/Ready';
+import { Client, Intents } from "discord.js-selfbot-v13";
+import message from "./events/MessageCreate";
+import ready from "./events/Ready";
 
 // Define DiscordJS v13 client.
-const client: Client = new Client({
-    intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
-    checkUpdate: false
+let client: Client = new Client({
+  intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
+  checkUpdate: false,
 });
 
-
-ready(client)
-message(client)
+// Start listeners
+// error();
+ready(client);
+message(client);
 
 client.login(process.env.TOKEN);
-
